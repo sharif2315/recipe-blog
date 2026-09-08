5a11079072bcf87fb9c2fd62d025969249116c7e
Sharif Ahmed <sharif2315@gmail.com>
2026-09-08 13:13:13 +0100

    added register, login, logout to routes

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,97 +1,61 @@
 import functools
+from urllib.parse import urlsplit
+
 from flask import flash, g, redirect, render_template, request, session, url_for
 from werkzeug.security import check_password_hash, generate_password_hash
-from app.db import get_db
+from flask_login import login_user, logout_user, current_user
+from flask_babel import _
+import sqlalchemy as sa
+
+from app import db
 from app.auth import bp
-from app.auth.forms import LoginForm
-
-
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-
-        if not username:
-            error = 'Username is required'
-        elif not password:
-            error = 'Password is required'
-
-        if error is None:
-            try:
-                db.execute(
-                    'INSERT INTO user (username, password) VALUES (?, ?)',
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for('auth.login'))
-
-        flash(error)
-    return render_template('auth/register.html')
-
-
-# @bp.route('/login', methods=('GET', 'POST'))
-# def login():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         db = get_db()
-#         error = None
-#         user = db.execute(
-#             'SELECT * FROM user WHERE username = ?', (username,)
-#         ).fetchone()
-
-#         if user is None:
-#             error = 'Incorrect username.'
-#         elif not check_password_hash(user['password'], password):
-#             error = 'Incorrect password.'
-
-#         if error is None:
-#             session.clear()
-#             session['user_id'] = user['id']
-#             return redirect(url_for('index'))
-
-#         flash(error)
-
-#     return render_template('auth/login.html')
-
+from app.auth.forms import (
+    LoginForm, RegistrationForm, ResetPasswordForm, ResetPasswordRequestForm
+)
+from app.models import User
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/dashboard')
-        
-    return render_template('auth/login.html', title='Sign In', form=form)
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash(_('Invalid username or password'))
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('main.home')
+        return redirect(next_page)
+    return render_template('auth/login.html', title=_('Sign In'), form=form)
 
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
 
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(_('Congratulations, you are now a registered user!'))
+        return redirect(url_for('auth.login'))
+
+    return render_template(
+        'auth/register.html', 
+        title=_('Register'),
+        form=form
+    )
 
 
 @bp.route('/logout')
 def logout():
-    session.clear()
-    return redirect(url_for('index'))
-
-
-def login_required(view):
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
-        return view(**kwargs)
-
-    return wrapped_view
+    logout_user()
+    return redirect(url_for('main.home'))
--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,12 +1,17 @@
 from flask import render_template
 from app.dashboard import bp
 from app.models import Recipe
+from flask_login import login_required
+
 
 @bp.route('/')
+@login_required
 def home():
     recipes = Recipe.query.all()
     return render_template('dashboard/index.html', recipes=recipes)
 
+
 @bp.route('/recipes/<int:recipe_id>')
+@login_required
 def recipe_detail(recipe_id):
     return render_template('dashboard/recipe_detail.html')
