e6b9cf17098fb4912540a24d5e8265fd63cd59ef
Sharif Ahmed <sharif2315@gmail.com>
2026-09-08 10:46:57 +0100

    displaying recipes in dashboard from db ctx

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template
 from app.dashboard import bp
-
+from app.models import Recipe
 
 @bp.route('/')
 def home():
-    return render_template('dashboard/index.html')
+    recipes = Recipe.query.all()
+    return render_template('dashboard/index.html', recipes=recipes)
 
 @bp.route('/recipes/<int:recipe_id>')
 def recipe_detail(recipe_id):
