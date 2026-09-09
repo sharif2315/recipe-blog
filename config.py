89f3a1988611370b17069769beab041e21f39d2f
Sharif Ahmed <sharif2315@gmail.com>
2026-09-09 13:12:10 +0100

    upgraded to use postgres migrations

--- config.py
+++ config.py
@@ -8,10 +8,17 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SERVER_NAME = os.environ.get('SERVER_NAME')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
-        'postgres://', 'postgresql://') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db'
-    )
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+    #     'postgres://', 'postgresql://') or \
+    #     'sqlite:///' + os.path.join(basedir, 'app.db'
+    # )
+    db_url = os.environ.get('DATABASE_URL')
+
+    if db_url and db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = db_url or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
